a47a9be524675536c059cdf2d673496770226d5a
carina11 <ryo.inagaki.ryo@gmail.com>
2019-11-30 11:14:18 +0000

    データのストアと取り出し可能

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );してください
-}*/
-/*function App (){
-  Text(){
-    alert()
-  }
-  return(
-    <div>
-      <input type="text" id="text" onChange="Text();" />
-      <input type="button" value="別の関数" onClick={()=>alert(`${document.getElementById("text").value}`)}/>
+const firebaseConfig = {
+  apiKey: "AIzaSyDNT2NvrxwArQrNZJQTozcveab7cH3ngf4",
+  authDomain: "ask-teacher-0211.firebaseapp.com",
+  databaseURL: "https://ask-teacher-0211.firebaseio.com",
+  projectId: "ask-teacher-0211",
+  storageBucket: "ask-teacher-0211.appspot.com",
+  messagingSenderId: "45700314741",
+  appId: "1:45700314741:web:ee03cc032881993b232a3c",
+  measurementId: "G-PD016DXDC5"
+};
 
-    </div>
-  );
-}*/
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 class App extends React.Component{
   text(){
     //alert(`${document.getElementById("kadai").value}${document.getElementById("subject").options[document.getElementById("subject").selectedIndex].value}`);
@@ -41,8 +25,23 @@ class App extends React.Component{
       "kadaiName": document.getElementById("kadai").value,
       "subject": document.getElementById("subject").options[document.getElementById("subject").selectedIndex].value
     };
-    console.log(json.kadaiName);
-    console.log(json.subject);
+    //console.log(json.kadaiName);
+    //console.log(json.subject);
+    var database = firebase.database().ref("test").push(json);
+    database.once("value",function(snapshot){
+      console.log(snapshot.val().kadaiName);
+      console.log(snapshot.val().subject);
+    });
+  }
+
+  search(){
+    var subject = document.getElementById("subjectSearch").options[document.getElementById("subjectSearch").selectedIndex].value;
+    var database = firebase.database().ref("test");
+    database.once("value", function(snapshot){
+      snapshot.forEach(function(childSnapshot){
+        if(childSnapshot.val().subject === subject)console.log(childSnapshot.val().kadaiName);
+      });
+    });
   }
   
   render(){
@@ -60,6 +59,18 @@ class App extends React.Component{
           <option value="japanese">国語</option>
         </select><br/><br/>
         <button onClick={this.text.bind(this)}>送信</button>
+        <br/><br/><br/>
+        科目検索<br/>
+        <select id="subjectSearch">
+          <option value=""></option>
+          <option value="english">英語</option>
+          <option value="math">数学</option>
+          <option value="social_studies">社会</option>
+          <option value="science">理科</option>
+          <option value="japanese">国語</option>
+        </select>
+        <br/>
+        <input type="button" value="検索" onClick={this.search.bind(this)}/>
       </div>
     );
   }
